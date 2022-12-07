eba09748d5d07544ff8ad5b834afabe064bf728c
Alexandre Diniz Veloso <alexandredinizveloso@gmail.com>
2022-12-06 21:21:06 -0300

    Guess game 3.0

--- 1_intro_to_py/guess_game.py
+++ 1_intro_to_py/guess_game.py
@@ -1,52 +1,30 @@
-import random
+print("\n*********************************")
+print("Bem vindo ao jogo de Adivinhação!")
+print("*********************************\n")
 
-def play():
-    print("\n*********************************")
-    print("Bem vindo ao jogo de Adivinhação!")
-    print("*********************************\n")
+secret_number = 42
+tries = 3
 
-    secret_number = random.randrange(1, 101)
-    ponto = 100
-    tent = 0
+for run in range(tries):
+    print("\nTentativa {} de {}".format(run+1, tries))
+    guess_str = input("Qual o seu chute? (Entre 1 e 100): ")
+    guess = int(guess_str)
 
-    print("Defina o nível de dificuldade")
-    dific = int(input("(1)Fácil (2)Médio (3)Difícil\n"))
+    correct = guess == secret_number
+    bigger = guess > secret_number
+    smaller = guess < secret_number
 
-    if dific == 1:
-        tent = 20
-    elif dific == 2:
-        tent = 10
+    if guess < 1 or guess > 100:
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
+
+    if correct:
+        print("Acertô miseravi!")
+        break
     else:
-        tent = 5
+        if bigger:
+            print("Errrrooouuu! O seu chute foi maior que o número secreto.")
+        elif smaller:
+            print("Errrrooouuu! O seu chute foi menor que o número secreto.")
 
-    for partida in range(tent):
-        print("\nTentativa {} de {}".format(partida+1, tent))
-        guess_str = input("Qual o seu chute? (Entre 1 e 100): ")
-        guess = int(guess_str)
-
-        certo = guess == secret_number
-        maior = guess > secret_number
-        menor = guess < secret_number
-
-        if guess < 1 or guess > 100:
-            print("Você deve digitar um número entre 1 e 100!")
-            points -= 10
-            continue
-
-        if certo:
-            print("Acertô miseravi!")
-            ponto += (tent - partida) * dific
-            break
-        else:
-            if maior:
-                print("Errrrooouuu! Tente um número menor")
-            elif menor:
-                print("Errrrooouuu! Tente um número maior")
-
-            ponto -= abs(secret_number - guess)
-
-    print(f"Pontuação: {ponto}")
-    print("\nFim do Jogo!")
-
-if __name__ == "__main__":
-    play()
+print("\nFim do Jogo!")
